7a201b6ccab12c9f8377cb128ecf1c3590357cee
Hashual <dodo291005@mail.com>
2023-09-29 14:12:20 +0200

    Initial commit

---
+++ extension_chrome.js
@@ -0,0 +1,8 @@
+let mot_de_passe = [];
+document.addEventListener("DOMContentLoaded",function(){
+    const envoyer = document.getElementById("envoyer");
+    envoyer.addEventListener("click",function(event){ 
+        console.log("ok")
+})
+})
+
