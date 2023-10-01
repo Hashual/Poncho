8f7ae31876ad75588011fba06e7198fc009d4d38
Hashual <dodo291005@mail.com>
2023-10-01 19:37:31 +0200

    Récupération formulaire

--- extension_chrome.js
+++ extension_chrome.js
@@ -1,8 +1,24 @@
 let mot_de_passe = [];
-document.addEventListener("DOMContentLoaded",function(){
-    const envoyer = document.getElementById("envoyer");
-    envoyer.addEventListener("click",function(event){ 
-        console.log("ok")
-})
-})
 
+document.addEventListener("DOMContentLoaded", function () {
+    const envoyer = document.getElementById("envoyer");
+    const motDePasseInput = document.getElementById("connection");
+
+    envoyer.addEventListener("click", function (event) {
+        event.preventDefault();
+        ajouterMotDePasse();
+    });
+
+    motDePasseInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            ajouterMotDePasse();
+        }
+    });
+
+    function ajouterMotDePasse() {
+        const motDePasseValue = motDePasseInput.value;
+        mot_de_passe.push(motDePasseValue);
+        console.log("Mots de passe enregistrés :", mot_de_passe);
+    }
+});
