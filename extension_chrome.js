f06c1c3ed8878766f407892c10368d654c1528c6
Hashual <dodo291005@mail.com>
2023-10-02 16:21:36 +0200

    start page

--- extension_chrome.js
+++ extension_chrome.js
@@ -2,17 +2,29 @@ let mot_de_passe = [];
 
 document.addEventListener("DOMContentLoaded", function () {
     const envoyer = document.getElementById("envoyer");
+<<<<<<< Updated upstream
     const motDePasseInput = document.getElementById("connection");
+=======
+    const motDePasseInput = document.getElementById("connexion");
+>>>>>>> Stashed changes
 
     envoyer.addEventListener("click", function (event) {
         event.preventDefault();
         ajouterMotDePasse();
+<<<<<<< Updated upstream
+=======
+        reinitialiserChampMotDePasse();
+>>>>>>> Stashed changes
     });
 
     motDePasseInput.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             event.preventDefault();
             ajouterMotDePasse();
+<<<<<<< Updated upstream
+=======
+            reinitialiserChampMotDePasse();
+>>>>>>> Stashed changes
         }
     });
 
@@ -21,4 +33,11 @@ document.addEventListener("DOMContentLoaded", function () {
         mot_de_passe.push(motDePasseValue);
         console.log("Mots de passe enregistrés :", mot_de_passe);
     }
+<<<<<<< Updated upstream
+=======
+
+    function reinitialiserChampMotDePasse() {
+        motDePasseInput.value = "";
+    }
+>>>>>>> Stashed changes
 });
