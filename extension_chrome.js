1fa55082e5b0aa17d1bd3168c30720a063337ee8
hugoheml <hugo-hermelin17@orange.fr>
2023-10-02 16:23:04 +0200

    fix: conflicts from branch change

--- extension_chrome.js
+++ extension_chrome.js
@@ -2,29 +2,19 @@ let mot_de_passe = [];
 
 document.addEventListener("DOMContentLoaded", function () {
     const envoyer = document.getElementById("envoyer");
-<<<<<<< Updated upstream
-    const motDePasseInput = document.getElementById("connection");
-=======
     const motDePasseInput = document.getElementById("connexion");
->>>>>>> Stashed changes
 
     envoyer.addEventListener("click", function (event) {
         event.preventDefault();
         ajouterMotDePasse();
-<<<<<<< Updated upstream
-=======
         reinitialiserChampMotDePasse();
->>>>>>> Stashed changes
     });
 
     motDePasseInput.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             event.preventDefault();
             ajouterMotDePasse();
-<<<<<<< Updated upstream
-=======
             reinitialiserChampMotDePasse();
->>>>>>> Stashed changes
         }
     });
 
@@ -33,11 +23,8 @@ document.addEventListener("DOMContentLoaded", function () {
         mot_de_passe.push(motDePasseValue);
         console.log("Mots de passe enregistrés :", mot_de_passe);
     }
-<<<<<<< Updated upstream
-=======
 
     function reinitialiserChampMotDePasse() {
         motDePasseInput.value = "";
     }
->>>>>>> Stashed changes
 });
